4e562c757d3583055d2fa8f5cc756e3807e5dffc
Charis <cgeo@mit.edu>
2024-11-18 22:13:00 -0500

    page setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+
+export default {
+    integrations: [],
+    vite: {
+      css: {
+        preprocessorOptions: {
+          css: {
+            importLoaders: 1,
+          },
+        },
+      },
+    },
+  };
+  
