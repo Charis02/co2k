c4245a55e79c569022baacc90abbd19e30dfc3d1
Charis <chge2002@gmail.com>
2024-11-18 22:33:10 -0500

    fix relative positions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-// export default defineConfig({});
-
-export default {
-    integrations: [],
-    vite: {
-      css: {
-        preprocessorOptions: {
-          css: {
-            importLoaders: 1,
-          },
+export default defineConfig({
+  integrations: [],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          importLoaders: 1,
         },
       },
     },
-    site: 'https://charis02.github.io',
-    base: 'co2k',
-  };
-  
+  },
+  site: 'https://charis02.github.io',
+  base: '/co2k',
+});
